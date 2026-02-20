762041f8dd6f6098ba1f3e86bd56a23134496554
Francesco Sommella <francescosommellaa@gmail.com>
2026-02-20 02:21:35 +0100

    fix db2

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,4 @@
 import { PrismaClient } from "../app/generated/prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+const prisma = new PrismaClient().$extends(withAccelerate());
